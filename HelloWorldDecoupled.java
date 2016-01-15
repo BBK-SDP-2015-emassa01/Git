d15003cdc9f21b2c6db535ff5de2417250fdd98a
Esha Massand <ubemas001@mail.bbk.ac.uk>
2016-01-15 13:47:11 +0000

    added a line

--- HelloWorldDecoupled.java
+++ HelloWorldDecoupled.java
@@ -1,11 +1,13 @@
 package decoupled;
 
 public class HelloWorldDecoupled {
+    
 
 	public static void main(String[] args) {
 		StandardOutMessageRenderer mr = new StandardOutMessageRenderer();
 		HelloWorldMessageProvider mp = new HelloWorldMessageProvider();
 		mr.setMessageProvider(mp);
 		mr.render();
+//        mr.render();
 	}
 }
